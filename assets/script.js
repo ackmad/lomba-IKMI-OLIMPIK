ce6812d7f51a195f713b5d1ce4f854b1d4ec92dc
informatika-elfan <ackmadelfanpurnama@smkinformatika-crbn.sch.id>
2025-01-12 15:52:02 +0700

    DONE : 10 jan 2025, 3.51

--- assets/script.js
+++ assets/script.js
@@ -4,3 +4,16 @@ const navMenus = document.getElementById('nav-menus');
 navbarToggle.addEventListener('click', () => {
     navMenus.classList.toggle('active');
 });
+
+
+function seedWhatsapp() {
+    var nama = document.getElementsByClassName('input-nama').value;
+    var pesan = document.getElementsByClassName('input-pesan').value;
+    var nomorWhatsApp = '6289666192392';
+
+    var url = `https://wa.me/${nomorWhatsApp}?text=` +
+              `Nama: ${encodeURIComponent(nama)}%0A` +
+              `Pesan: ${encodeURIComponent(pesan)}`;
+
+    window.open(url, '_blank');
+}
