2151ab54c3f997c6e5cee3b69faf00a97c694d34
informatika-elfan <ackmadelfanpurnama@smkinformatika-crbn.sch.id>
2025-01-12 18:11:12 +0700

    UPDATE : add link and fix hero

--- assets/script.js
+++ assets/script.js
@@ -12,8 +12,29 @@ function seedWhatsapp() {
     var nomorWhatsApp = '6289666192392';
 
     var url = `https://wa.me/${nomorWhatsApp}?text=` +
-              `Nama: ${encodeURIComponent(nama)}%0A` +
-              `Pesan: ${encodeURIComponent(pesan)}`;
+        `Nama: ${encodeURIComponent(nama)}%0A` +
+        `Pesan: ${encodeURIComponent(pesan)}`;
 
+    window.open(url, '_blank');
+}
+
+function cahaya_cinta() {
+    var nomorWhatsApp = '6289666192392';
+    var pesan = encodeURIComponent("Hallo! Saya tertarik dengan paket pernikahan CAHAYA CINTA yang Anda tawarkan. Bisakah Anda memberikan informasi lebih lanjut dan detail paket yang tersedia? Terima kasih!");
+    var url = `https://wa.me/${nomorWhatsApp}?text=${pesan}`;
+    window.open(url, '_blank');
+}
+
+function harmoni_tradisi() {
+    var nomorWhatsApp = '6289666192392';
+    var pesan = encodeURIComponent("Hallo! Saya tertarik dengan paket pernikahan HARMONI TRADISI yang Anda tawarkan. Bisakah Anda memberikan informasi lebih lanjut dan detail paket yang tersedia? Terima kasih!");
+    var url = `https://wa.me/${nomorWhatsApp}?text=${pesan}`;
+    window.open(url, '_blank');
+}
+
+function mewaak_eksklusif() {
+    var nomorWhatsApp = '6289666192392';
+    var pesan = encodeURIComponent("Hallo! Saya tertarik dengan paket pernikahan MEWAH EKSKLUSIF yang Anda tawarkan. Bisakah Anda memberikan informasi lebih lanjut dan detail paket yang tersedia? Terima kasih!");
+    var url = `https://wa.me/${nomorWhatsApp}?text=${pesan}`;
     window.open(url, '_blank');
 }
